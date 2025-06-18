75c27605800c17e5238e586be0f804c26bb45adc
Andrey <a.e.hvatov@gmail.com>
2025-06-19 01:03:55 +0500

    Отдельный класс и раскидал методы в ДЗ3

---
+++ Contact.py
@@ -0,0 +1,25 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home,
+                 mobile, work, fax, email, email2, email3, homepage, byear, ayear, bday, bmonth, aday, amonth):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.bday = bday
+        self.bmonth = bmonth
+        self.aday = aday
+        self.amonth = amonth
+
