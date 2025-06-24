8fd07889744f16dabc64db229a0260e983c96c7a
Andrey <a.e.hvatov@gmail.com>
2025-06-24 20:34:16 +0500

    добавил метод удаления контакта

---
+++ test/test_delete_contact.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model._сontact import Contact
+
+
+def test_delete_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.delete()
+    app.session.logout()
