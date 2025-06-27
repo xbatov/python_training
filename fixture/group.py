56b57944e1b9d3d42dd0d23b0706126174732f91
Hvatov <a.hvatov@kontur.ru>
2025-06-27 18:15:44 +0500

    Повысил читаемость, избавился от дублирования кода при изменении группы.

--- fixture/group.py
+++ fixture/group.py
@@ -7,46 +7,48 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def change_field_value(self, field_name, text):
+            wd = self.app.wd
+            if text is not None:
+                wd.find_element_by_name(field_name).click()
+                wd.find_element_by_name(field_name).clear()
+                wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group.header", group.header)
+        self.change_field_value("group.footer", group.footer)
+
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.return_groups_page()
 
-    def edit_first_group(self):
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        #поставили галку у верхней группы
-        wd.find_element_by_name("selected[]").click()
-        #нажали на кнопку Edit
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("123")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("123")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("123")
+
+        self.fill_group_form(self, new_group_data)
+        
         wd.find_element_by_name("update").click()
+        self.return_groups_page()
 
     def return_groups_page(self):
         wd = self.app.wd
--- test/test_edit_group.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group()
-    app.session.logout()
