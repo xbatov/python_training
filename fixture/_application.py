743eb0e827c4bfcadbb1aeba5fd9a6827d205728
Andrey <a.e.hvatov@gmail.com>
2025-07-15 23:52:19 +0500

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/_application.py
+++ fixture/_application.py
@@ -6,11 +6,19 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %S" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper (self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- test/conftest.py
+++ test/conftest.py
@@ -6,12 +6,14 @@ fixture = None
 @pytest.fixture ()
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
         fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
-                fixture = Application()
+                fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -22,3 +24,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
