91cd7509897661acd2b1581b2cb20e2a9a9553a4
Andrey <a.e.hvatov@gmail.com>
2025-07-22 11:46:35 +0500

    Задание №18: Отделение тестовых данных и их генерация в формате json для теста добавления группы.

---
+++ data/groups.py
@@ -0,0 +1,6 @@
+from model._group import Group
+
+testdata = [
+    Group(name="name1", header="header1", footer="footer1"),
+    Group(name="name2", header="header2", footer="footer2")
+]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
 from model._group import Group
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(2)
-]
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
