c9376b2da56c5930a134a45b56a93f4b870b1f23
Andrey <a.e.hvatov@gmail.com>
2025-07-29 22:17:08 +0500

    Задание №21: Переделать тесты для проверки информации о контактах на главной странице

--- test/test_check_ui_db_contacts.py
+++ test/test_check_ui_db_contacts.py
@@ -2,18 +2,10 @@ from model._contact import Contact
 
 def test_check_ui_db_contacts(app, db):
     if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(
-            firstname="firstname",
-            middlename="middlename",
-            lastname="lastname",
-            address="address",
-            homephone="111",
-            mobilephone="222",
-            workphone="333",
-            email="email",
-            email2="email2",
-            email3="email3"
-        ))
+        app.contact.create(Contact(firstname="firstname", middlename="middlename", lastname="lastname", nickname="", title="",
+                                   company="", address="address", homephone="111", mobilephone="222", workphone="333", fax="", email="email",
+                                   email2="email2", email3="email3", homepage="", byear="1900", ayear="2000", bday="24", bmonth="May",
+                                   aday="14", amonth="December"))
     ui_contacts = app.contact.get_contact_list()
     db_contacts = db.get_contact_list()
     assert sorted(ui_contacts, key=Contact.id_or_max) == sorted(db_contacts, key=Contact.id_or_max)
